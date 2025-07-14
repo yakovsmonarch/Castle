6e5fd6dc991ba02b8f441aca1a902e29fc8e25cb
yakovsmonarch <frag.2010@yandex.ru>
2025-07-14 21:20:39 +0800

    Сбор монеток игроком.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Transform _playerTransform;
+    private CoinNumber _coinNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerTransform = FindObjectOfType<Person>().transform;
+        _coinNumber = FindObjectOfType<CoinNumber>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, _playerTransform.position);
+        if (distance < 2f)
+        {
+            _coinNumber.Number += 1;
+            Destroy(gameObject);
+        }
+    }
+}
