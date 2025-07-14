26fbb573e96bed53cf98253499a27c2f9d0d9758
yakovsmonarch <frag.2010@yandex.ru>
2025-07-14 18:07:56 +0800

    Изменения цвета материала в таймере.

--- Assets/Scripts/Pig.cs
+++ Assets/Scripts/Pig.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Pig : MonoBehaviour
 {
+    public Material PigMaterial;
+    public float Timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,24 @@ public class Pig : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Timer += Time.deltaTime;
+
+        if (Timer > 15)
+        {
+            Timer = 0;
+        }
+
+        if (Timer < 3)
+        {
+            PigMaterial.color = Color.green;
+        }
+        else if (Timer < 6)
+        {
+            PigMaterial.color = Color.yellow;
+        }
+        else
+        {
+            PigMaterial.color = Color.red;
+        }
     }
 }
