6cdd0ab5c897d4154a1f03c5f8bb8a614fcfee07
yakovsmonarch <frag.2010@yandex.ru>
2025-07-15 12:41:42 +0800

    Сообщение о сборе все монеток.

--- Assets/Scripts/CoinNumber.cs
+++ Assets/Scripts/CoinNumber.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinNumber : MonoBehaviour
 {
     public int Number;
+    public Canvas MyCanvas;
+    private int _coinNumber;
+
+    private void Start()
+    {
+        _coinNumber = FindObjectsOfType<Coin>().Length;
+        Debug.Log($"Число монеток: {_coinNumber}");
+    }
+
+    private void Update()
+    {
+        if (Number >= _coinNumber)
+        {
+            MyCanvas.gameObject.SetActive(true);
+        }
+    }
 }
